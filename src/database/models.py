4161e6f54e8f6654b924a676ef84489db9765bbc
Anastasia <ashevchenko629@gmail.com>
2024-01-14 19:03:24 +0100

    db

---
+++ src/database/models.py
@@ -0,0 +1,30 @@
+import enum
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, Integer, ForeignKey, DateTime, func, Enum, Column, Boolean
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
+    role: Mapped[Enum] = mapped_column("role", Enum(Role), default=Role.user, nullable=True)
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)

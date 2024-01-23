74338708c9ee1819ca5766e540401f4080f51231
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-01-23 18:56:15 +0200

    Update photo_service.py

--- src/services/photo_service.py
+++ src/services/photo_service.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.models import Photo
+from sqlalchemy.future import select
 
 async def get_photo_by_id(photo_id: int, db: AsyncSession):
-    photo = await db.execute(
-        Photo.select().where(Photo.id == photo_id)
-    )
+    stmt = select(Photo).where(Photo.id == photo_id)
+    result = await db.execute(stmt)
     
-    return photo.scalar()
+    return result.scalar_one()
